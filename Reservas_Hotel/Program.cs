d8bf17d858cf98e8da429a76c21e4492466c2cda
Maluh2020 <maluhmyth@gmail.com>
2020-12-08 17:25:33 -0300

    first commit

---
+++ Reservas_Hotel/Program.cs
@@ -0,0 +1,29 @@
+﻿using Reservas_Hotel.Entities;
+using System;
+
+namespace Reservas_Hotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Room number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date( dd/MM/yyyy)");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date( dd/MM/yyyy)");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            if(checkOut <= checkIn)
+            {
+                Console.WriteLine("Erro in resrvation: Check-out date must be after check-in date");
+            }
+            else
+            {
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+            }
+
+        }
+    }
+}
